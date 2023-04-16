6534d121fc439a6bd7344e3b179767565e871cfd
Renjithkumar993 <121647888+Renjithkumar993@users.noreply.github.com>
2023-04-16 17:22:56 -0300

    added app functionality

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -107,8 +107,19 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+router.delete('/:id',async (req, res) => {
+ 
+  try{
+    const delProduct  = await Product.destroy({
+      where:{
+        id:req.params.id
+      }
+    })
+    res.status(200).json(delProduct)
+  }
+  catch(err){
+     res.status(500).json(err)
+  }
 });
 
 module.exports = router;
